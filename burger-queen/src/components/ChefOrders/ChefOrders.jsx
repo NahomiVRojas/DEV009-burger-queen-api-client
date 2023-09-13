377d0c25db7482abe4d05ca9e2dd842f8b720fc9
AndressaSRodrigues <srandressa2@gmail.com>
2023-09-13 19:27:33 -0300

    Checkbox style

---
+++ burger-queen/src/components/ChefOrders/ChefOrders.jsx
@@ -0,0 +1,107 @@
+import style from "../ChefOrders/ChefOrders.module.css";
+import returnButton from "../../assets/return-button.svg";
+import NavigateTo from "../Navigate/Navigate";
+import React, { useState, useEffect } from "react";
+import { allOrders } from "../../Services/Request";
+import iconShowInfo from "../../assets/icon-show-info.svg";
+
+export default function ChefOrders() {
+    const token = localStorage.getItem("token");
+    const handleClick = NavigateTo("/admin/dashboard");
+    const [orders, setOrders] = useState([]);
+    const [expandedOrder, setExpandedOrder] = useState(null);
+    const [duration, setDuration] = useState([]);
+
+    useEffect(() => {
+        allOrders(token)
+            .then((response) => {
+                console.log("Response allOrders:", response);
+                if (!response.ok) {
+                    throw new Error("No orders available.");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Data getProducts:", data);
+                setOrders(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [token]);
+    const toggleExpand = (orderId) => {
+        setExpandedOrder((prevExpandedOrder) =>
+            prevExpandedOrder === orderId ? null : orderId
+        );
+    };
+
+    return (
+        <>
+            <div className={style.title_section}>
+                <div className={style.title}>
+                    <img src={returnButton} onClick={handleClick} alt="Return" />
+                    <h2>Active Orders</h2>
+                </div>
+            </div>
+            <div className={`table-responsive ${style.responsive}`}>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Table</th>
+                            <th>Received</th>
+                            <th>Status</th>
+                            <th>Delivered</th>
+                            <th>Duration</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order, index) => {
+                            const isExpanded = expandedOrder === order.id;
+                            return (
+                                <React.Fragment key={index}>
+                                    <tr onClick={() => toggleExpand(order.id)}>
+                                        <td>
+                                            <img src={iconShowInfo} />
+                                        </td>
+                                        <td>{order.table}</td>
+                                        <td>{order.dataEntry}</td>
+                                        <td>{order.status}</td>
+                                        <td>{order.dateProcessed}</td>
+                                        <td>{duration}</td>
+                                        <td>
+                                            <button className={style.button}>Start</button>
+                                        </td>
+                                    </tr>
+                                    {isExpanded && (
+                                        <tr
+                                            key={`expanded_${order.id}`}
+                                            className={style.expanded_row}
+                                        >
+                                            <td colSpan="7" className={style.expanded_info}>
+                                                <ul>
+                                                    {order.products.map((product, productIndex) => (
+                                                        <li key={productIndex} className={style.expanded_list}>
+                                                            <input
+                                                                className={style.checkbox}
+                                                                type="checkbox"
+                                                            />
+                                                            <label className={style.label}>
+                                                                {product.name}
+                                                            </label>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    )}
+                                </React.Fragment>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
