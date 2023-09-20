4137c7bb6d9160abd7eb94e91731d01b5113f14d
AndressaSRodrigues <srandressa2@gmail.com>
2023-09-20 17:33:40 -0300

    Test config

---
+++ burger-queen/jest.config.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+    },
+    transformIgnorePatterns: [
+      "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    ],
+  };
